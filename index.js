650e808e90530731a746a2bb9ce170953da9293a
clenda <glendabittok24@gmail.com>
2024-04-17 16:23:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector('main#main')
+document.body.removeChild(main)
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Clenda is the champion'
+document.body.appendChild(newHeader)
